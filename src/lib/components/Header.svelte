70d2a4a02e1fdefd57eb85dd735c3762064a6132
Hyewon Im <hyewon.im@icloud.com>
2025-03-17 12:26:30 +0000

    Add header, footer, and layout components with basic navigation

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a 
+                    href="/" 
+                    class:active={$page.url.pathname === '/'}
+                >
+                    Home
+                </a>
+            </li>
+            <li>
+                <a 
+                    href="/work" 
+                    class:active={$page.url.pathname === '/work'}
+                >
+                    Blog
+                </a>
+            </li>
+            <li>
+                <a 
+                    href="/contact" 
+                    class:active={$page.url.pathname === '/contact'}
+                >
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+    import '$lib/styles/style.css';
+	import Footer from '$lib/components/Footer.svelte'
+</script>
+
+
+
+<main>
+	<Header />
+	<slot />
+	<Footer />
+</main>
